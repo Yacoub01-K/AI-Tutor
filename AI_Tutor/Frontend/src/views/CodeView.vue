936f29b0cb9ed4e9b1afacbdaaafae9b93b77e41
Yacoub01-K <yalkaradsheh@gmail.com>
2024-04-11 18:46:26 +0100

    basic skeleton and ui for ai and class

---
+++ AI_Tutor/Frontend/src/views/CodeView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Problem Sheet</h1>
+        <Code-Editor />
+    </div>
+</template>
+
+<script>
+import CodeEditor from '@/components/CodeEditor.vue';
+
+export default {
+    components: {
+        CodeEditor,
+    },
+};
+</script>
